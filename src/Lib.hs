992e6c6f9f151a87a1e8aa4fb6f038be02325b8f
Chris Ford <christophertford@gmail.com>
2025-09-12 17:24:42 +0200

    Use $ consistently in server handlers

--- src/Lib.hs
+++ src/Lib.hs
@@ -228,11 +228,11 @@ api = Proxy
 
 -- Server implementation using graded monads
 server :: NumberState -> Server API
-server state = handle (showNumber state)
+server state = handle $ showNumber state
           :<|> (\(NumberRequest n) -> handle $ setNumber state n)
           :<|> (\(NumberRequest n) -> handle $ addNumber state n)
-          :<|> handle (randomiseNumber state)
-          :<|> handle (resetNumber state)
+          :<|> handle $ randomiseNumber state
+          :<|> handle $ resetNumber state
           :<|> staticHandler
   where
     handle = liftIO . runAction
